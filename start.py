4fe205742682f0355ecbaf4c2ec36dcd108bde67
Jonathan Chamberlin <jcham17x@gmail.com>
2025-11-13 21:48:44 -0500

    first

---
+++ start.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+trips = pd.read_csv(r"C:\Repositories for Git\uber-trips-analysis-folder\uber-raw-data-sep14.csv")
+
+row_10 = trips.iloc[9]
+
+print(row_10)
+
+#Problem 1: find busiest date
+# LEFT OFF[][][]
+# Problem 2: Find time with lowest number of rides
