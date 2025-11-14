c1b494f640e19b4ebb49fa3054ac8c4cb070e3fb
Jonathan Chamberlin <jcham17x@gmail.com>
2025-11-14 12:00:13 -0500

    problem 1

--- start.py
+++ start.py
@@ -2,10 +2,23 @@ import pandas as pd
 
 trips = pd.read_csv(r"C:\Repositories for Git\uber-trips-analysis-folder\uber-raw-data-sep14.csv")
 
-row_10 = trips.iloc[9]
 
-print(row_10)
 
 #Problem 1: find busiest date
-# LEFT OFF[][][]
+
+trips_by_day = trips.groupby('Date/Time')
+
+
+trips['Date'] = trips['Date/Time'].apply(lambda n: n.split(' ')[0])
+    # Adds a column to the trips table
+
+busiest_date = trips['Date'].value_counts().idxmax()
+most_trips_in_one_day = trips['Date'].value_counts().iloc[0]
+
+busiest_day_message = f"The busiest date was {busiest_date}. It had {most_trips_in_one_day} trips."
+
+print(busiest_day_message)
+
+
+
 # Problem 2: Find time with lowest number of rides
