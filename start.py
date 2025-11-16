bf0b6284e7f2e10b5013de5edebf2c1f5baccd5b
Jonathan Chamberlin <jcham17x@gmail.com>
2025-11-16 14:12:31 -0500

    problem 4

--- start.py
+++ start.py
@@ -25,7 +25,7 @@ least_busy_date_message = f"The least date was {least_busy_date}. It had {least_
 
 print(least_busy_date_message)
 
-# Problem 3: Find Busiest hours of the day
+# Problem 3: Find Busiest time of the day
 trips['Time'] = trips['Date/Time'].apply(lambda n: n.split(' ')[1])
 #build column 'Time' as the time of the trip 
 
@@ -36,9 +36,13 @@ busiest_minute_message = f"The busiest minute was {busiest_minute}. Across the d
 
 print(busiest_minute_message)
 
-# Expected is 18:12:00    1380
+# Problem 4: Find Slowest time of the day
+least_busy_minute = trips['Time'].value_counts().idxmin()
+trips_during_least_busy_minute = trips['Time'].value_counts().min()
 
-# Problem 4: Find Slowest hours of the day
+least_busy_minute_message = f"The least busy minute was {least_busy_minute}. Across the dataset, there were {trips_during_least_busy_minute} that time."
+
+print(least_busy_minute_message)
 
 # Problem 5: Find Most popular pickup areas (cluster Lat/Lon coordinates)
 
